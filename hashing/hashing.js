3f42e3470ba5477fcb2b6ad1b7eb2fd07d3ec1b7
tankicag <gonevatankica@gmail.com>
2022-05-06 18:41:16 +0100

    solution to hashing exercise (part one only)

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -5,39 +5,59 @@ var crypto = require("crypto");
 // The Power of a Smile
 // by Tupac Shakur
 var poem = [
-	"The power of a gun can kill",
-	"and the power of fire can burn",
-	"the power of wind can chill",
-	"and the power of a mind can learn",
-	"the power of anger can rage",
-	"inside until it tears u apart",
-	"but the power of a smile",
-	"especially yours can heal a frozen heart",
+  "The power of a gun can kill",
+  "and the power of fire can burn",
+  "the power of wind can chill",
+  "and the power of a mind can learn",
+  "the power of anger can rage",
+  "inside until it tears u apart",
+  "but the power of a smile",
+  "especially yours can heal a frozen heart",
 ];
 
 var Blockchain = {
-	blocks: [],
+  blocks: [],
 };
 
 // Genesis block
 Blockchain.blocks.push({
-	index: 0,
-	hash: "000000",
-	data: "",
-	timestamp: Date.now(),
+  index: 0,
+  hash: "000000",
+  data: "",
+  timestamp: Date.now(),
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock(line)
+}
 
 // console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
+// create createBlock() function
+function createBlock(text) {
+  let block = {
+    index: Blockchain.blocks.length,
+    prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+    data: text,
+    timestamp: Date.now(),
+  };
+
+  block.hash = blockHash(block);
+  Blockchain.blocks.push(block);
+
+  console.log(block);
+  return block;
+}
 
 // **********************************
 
 function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
+  return crypto
+    .createHash("sha256")
+    .update(
+      // TODO: use block data to calculate hash
+      `${bl.index}; ${bl.prevHash}; ${bl.data}; ${bl.timestamp};`
+    )
+    .digest("hex");
 }
